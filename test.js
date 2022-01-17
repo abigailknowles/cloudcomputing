61f1a0d570d0b032e921fd397b2e2c379f5c280f
abigailknowles <abigailknowlesx@yahoo.co.uk>
2022-01-17 11:22:29 +0000

    additional tasks

--- test.js
+++ test.js
@@ -7,15 +7,15 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello Virtual Machine!')
 })
+app.get('/abbie', (req, res) => {
+    res.send('Hello Abbie')
+})
 
 app.post('/name', (req, res) => {
     var name = req.body.name;
     res.send('Hello' + name)
 })
 
-app.get('/abbie', (req, res) => {
-    res.send('Getting a post request')
-})
 
 app.listen(port, () => {
     console.log(`Express Application  listening at port 3000`)
